42123e8aefd678453dc57687f0c656eedaeaefd2
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-07 16:41:38 -0700

    updated covid dashboard to fit better in card. added id to search button. changed a few js ids to match

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var tableBody = document.getElementById('results');
-var fetchButton = document.getElementById('get-button');
+var fetchButton = document.getElementById('search-button'); // changed id to match with html -bt
 
 function getApi(){
-    var searchField = document.getElementById('searchField');
+    var searchField = document.getElementById('search'); // changed id to match with html -bt
 
     var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?city='+searchField.value+'&apikey=Enbe7EvmaJcFDISS11Qk5cMImrLCPkBI';
 
